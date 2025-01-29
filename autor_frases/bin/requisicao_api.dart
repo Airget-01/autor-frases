b5c7e12df0b670e904ed889d824646763db7a41c
Airget-01 <dapouco3@gmail.com>
2025-01-29 12:38:53 -0300

    model made

--- autor_frases/bin/requisicao_api.dart
+++ autor_frases/bin/requisicao_api.dart
@@ -1,9 +1,41 @@
 import 'package:http/http.dart' as http;
+
 void main(List<String> args) {
   fecth();
 }
-Future <void> fecth() async {
+
+Future<void> fecth() async {
   String url = 'https://jsonplaceholder.typicode.com/todos/1';
   var response = await http.get(Uri.parse(url));
   print(response.body);
+}
+
+class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  Todo(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.completed});
+
+  factory Todo.MapJson(Map Json) {
+    return Todo(
+      completed: Json['completed'],
+      userId: Json['userId'],
+      id: Json['id'],
+      title: Json['title'],
+    );
+  }
+  Map toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'userId': userId,
+      'completed': completed,
+    };
+  }
 }
