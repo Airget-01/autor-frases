7d2804d4de1d7eedbc154350c471692b6b758685
Airget-01 <dapouco3@gmail.com>
2025-01-31 20:50:43 -0300

    apaguei tudo sobre a aula e agora vou fazer sozinho

--- autor_frases/lib/main.dart
+++ autor_frases/lib/main.dart
@@ -4,3 +4,7 @@ void main() {
   runApp(const MaterialApp());
 }
 
+//usar art.ai para criar uma imagem do rosto olhando para camera
+//depois reunir fotos de todo o corpo,que sejam parecidos
+//logo apos ir no discord e usar um bot face Swap
+//
